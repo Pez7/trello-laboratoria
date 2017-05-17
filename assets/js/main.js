edb89f98ca7fe009883fd966c5371996e5d69bd6
Maria Paz <maria.pazrojasc@gmail.com>
2017-05-17 16:24:36 -0400

    comentario2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,28 @@
- var nuevaTarea = document.getElementById("nueva-tarea");
- var contenedor = document.getElementById("contenedor");
+var nuevaTarea = document.getElementById("nueva-tarea");
+var contenedor = document.getElementById("contenedor");
+var caja = document.createElement('div');
+var entrada = document.createElement('input');
+var boton = document.createElement('button');
 
  nuevaTarea.addEventListener("click", function(){
-
- 	var caja = document.createElement('div');
-	var entrada = document.createElement('input');
-	var boton = document.createElement('button');
-	var texto = document.createTextNode("Añadir tarea");
+	/*var txt1 = document.createTextNode("")*/
+	var txt = document.createTextNode("Añadir tarea");
 
 	caja.appendChild(entrada);
 	caja.appendChild(boton);
 	contenedor.appendChild(caja);
-	boton.appendChild(texto);
-	
+	boton.appendChild(txt);
+
 });
+
+ boton.setAttribute("id","agregaTarea");
+
+ var agrTarea = document.getElementById("agregarTarea");
+
+ 	agrTarea.addEventListener("click", function(){
+ 		alert("Hola Mundo");
+
+ });
+ entrada.value;
 
  
